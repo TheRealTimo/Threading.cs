8b057258b0a86ecb15a1202b1360c77831cf96e4
Timo <31155789+TheRealTimo@users.noreply.github.com>
2023-03-23 22:17:16 +0100

    Amazon Scraper
    Initial Amazon scraper that uses all threading techniques discussed in the start doc. Still not using database etc

--- market-scraper/market-scraper/MainPage.xaml.cs
+++ market-scraper/market-scraper/MainPage.xaml.cs
@@ -1,31 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
 
 namespace market_scraper
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
-            Console.WriteLine("DAB");
         }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = SearchTermTextBox.Text;
+            int maxThreads = int.Parse(MaxThreadsTextBox.Text);
+            int pageNum = int.Parse(PageNumTextBox.Text);
+
+
+            var products = await FetchAmazonPrices(searchTerm, maxThreads, pageNum);
+
+            ProductsDataGrid.ItemsSource = products;
+        }
+
+        private async Task<List<AmazonProduct>> FetchAmazonPrices(string searchTerm, int maxThreads, int pageNum)
+        {
+            var products = new List<AmazonProduct>();
+            var sem = new SemaphoreSlim(maxThreads);
+
+            
+
+            async Task ScrapePage(int page)
+            {
+                await sem.WaitAsync();
+                try
+                {
+                    string amazonUrl = $"https://www.amazon.nl/s?k={searchTerm}&page={page}";
+
+                    var web = new HtmlWeb();
+                    var doc = await web.LoadFromWebAsync(amazonUrl);
+
+                    var items = doc.DocumentNode.QuerySelectorAll(".s-result-item");
+
+                    foreach (var item in items)
+                    {
+                        string title = item.QuerySelector(".a-text-normal")?.InnerText.Trim();
+                        string price = item.QuerySelector(".a-price-whole")?.InnerText.Trim();
+
+                        if (title != null && price != null)
+                        {
+                            lock (products)
+                            {
+                                products.Add(new AmazonProduct { Name = title, Price = "€" + price });
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    sem.Release();
+                }
+            }
+
+            var tasks = new List<Task>();
+
+            for (int i = 1; i <= pageNum; i++)
+            {
+                tasks.Add(ScrapePage(i));
+            }
+
+            await Task.WhenAll(tasks);
+
+            return products;
+        }
+    }
+
+    public class AmazonProduct
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
     }
 }
