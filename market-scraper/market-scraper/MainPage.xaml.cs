fb1ebee1d5d13fde4550187254a24f09b97b55dc
Timo <31155789+TheRealTimo@users.noreply.github.com>
2023-03-27 21:58:38 +0200

    Database
    Implemented a local database and connection. Tested with the amazonScraper implementation

--- market-scraper/market-scraper/MainPage.xaml.cs
+++ market-scraper/market-scraper/MainPage.xaml.cs
@@ -14,9 +14,12 @@ namespace market_scraper
 {
     public sealed partial class MainPage : Page
     {
+        private readonly Database _database;
+        readonly CultureInfo _culture = new CultureInfo("nl-NL");
         public MainPage()
         {
             this.InitializeComponent();
+            _database = new Database();
         }
 
         private async void SearchButton_Click(object sender, RoutedEventArgs e)
@@ -29,23 +32,55 @@ namespace market_scraper
             var products = await FetchAmazonPrices(searchTerm, maxThreads, pageNum);
             ProductsDataGrid.ItemsSource = products;
             
-            CultureInfo culture = new CultureInfo("nl-NL");
-            //CultureInfo needed to let the system know that the decimal separator is a comma and not a dot on amazon
-            
-            double minPrice = products.Min(p => double.Parse(p.Price.Replace("€", ""), culture));
-            double maxPrice = products.Max(p => double.Parse(p.Price.Replace("€", ""), culture));
-            double avgPrice = products.Average(p => double.Parse(p.Price.Replace("€", ""), culture));
-            
-            MinPriceTextBlock.Text += minPrice.ToString("0.00", culture);
-            MaxPriceTextBlock.Text += maxPrice.ToString("0.00", culture);
-            AvgPriceTextBlock.Text += avgPrice.ToString("0.00", culture);
+
+        }
+        private async void LoadDataButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = SearchTermTextBox.Text;
+            string platform = "Amazon"; // Replace with the platform you want to retrieve data for
+
+            // Create a new instance of the Database class
+            var database = new Database();
+
+            // Get the products from the database based on searchTerm and platform
+            var products = await database.GetProductsBySearchTermAndPlatformAsync(searchTerm, platform);
+
+            // Bind the retrieved products to the ProductsDataGrid
+            ProductsDataGrid.ItemsSource = products;
+            DisplayProductData();
         }
 
-        private async Task<List<AmazonProduct>> FetchAmazonPrices(string searchTerm, int maxThreads, int pageNum)
+        private async void DisplayProductData()
         {
-            var products = new List<AmazonProduct>();
-            var sem = new SemaphoreSlim(maxThreads);
+            var searchTerm = SearchTermTextBox.Text;
+            var platform = "Amazon";
+
+            var products = await _database.GetProductsBySearchTermAndPlatformAsync(searchTerm, platform);
+            ProductsDataGrid.ItemsSource = products;
+
             
+
+            double minPrice = products.Min(p => p.ProductPrice);
+            double maxPrice = products.Max(p => p.ProductPrice);
+            double avgPrice = products.Average(p => p.ProductPrice);
+
+            MinPriceTextBlock.Text = "Minimum Price: " + minPrice.ToString("0.00", _culture);
+            MaxPriceTextBlock.Text = "Maximum Price: " + maxPrice.ToString("0.00", _culture);
+            AvgPriceTextBlock.Text = "Average Price: " + avgPrice.ToString("0.00", _culture);
+        }
+        
+        private async void ClearDatabaseButton_Click(object sender, RoutedEventArgs e)
+        {
+            await _database.ClearProductsAsync();
+        }
+
+
+
+        private async Task<List<Product>> FetchAmazonPrices(string searchTerm, int maxThreads, int pageNum)
+        {
+            var products = new List<Product>();
+            var sem = new SemaphoreSlim(maxThreads);
+
             async Task ScrapePage(int page)
             {
                 await sem.WaitAsync();
@@ -65,9 +100,25 @@ namespace market_scraper
 
                         if (title != null && price != null)
                         {
+                            
+                            double productPrice = double.Parse(price.Replace("€", ""), NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, _culture);
+
+
+                            var product = new Product
+                            {
+                                ProductName = title,
+                                ProductPrice = productPrice,
+                                Platform = "Amazon",
+                                SearchTerm = searchTerm,
+                                Date = DateTime.UtcNow
+                            };
+
+                            // Save the product to the database
+                            await _database.SaveProductAsync(product);
+
                             lock (products)
                             {
-                                products.Add(new AmazonProduct { Name = title, Price = "€" + price });
+                                products.Add(product);
                             }
                         }
                     }
@@ -89,5 +140,7 @@ namespace market_scraper
 
             return products;
         }
+
+
     }
 }
