891fe2b9ae371456021b7edbff42c46954bab426
tadas0420 <tadas0420@gmail.com>
2023-03-27 10:41:26 +0200

    t

--- market-scraper/market-scraper/MainPage.xaml.cs
+++ market-scraper/market-scraper/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -17,34 +18,96 @@ namespace market_scraper
 
         private async void btnRetrieveData_Click(object sender, RoutedEventArgs e)
         {
-            string soldItemsUrl = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=air+jordan+1+bred&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1";
+            string itemName = txtSearchItem.Text.Trim();
+            bool searchSoldListings = chkSold.IsChecked == true;
+            bool searchActiveListings = chkActive.IsChecked == true;
+            int maxThreads = int.Parse(MaxThreadsTextBox.Text);
+            int pageNum = int.Parse(PageNumTextBox.Text);
 
-            // Send a GET request to the sold items page
+            if (itemName.Length == 0 || (!searchSoldListings && !searchActiveListings))
+            {
+                tbSettingText.Text = "Please enter an item name and select a search type.";
+                return;
+            }
+
+            string ebayUrl = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=" + itemName.Replace(" ", "+") + "&_sacat=0";
+            if (searchSoldListings)
+            {
+                ebayUrl += "&LH_Sold=1&LH_Complete=1";
+            }
+            else if (searchActiveListings)
+            {
+                ebayUrl += "&LH_Auction=1&LH_Sale_Currency=0&LH_Complete=1";
+            }
+
             using (HttpClient client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0");
                 client.DefaultRequestHeaders.Add("Referer", "https://www.ebay.com/");
-                var response = await client.GetAsync(soldItemsUrl);
-                response.EnsureSuccessStatusCode();
-                string soldItemsHtml = await response.Content.ReadAsStringAsync();
-                
-                HtmlDocument soldItemsDoc = new HtmlDocument();
-                soldItemsDoc.LoadHtml(soldItemsHtml);
-                
-                List<double> soldItemPrices = new List<double>();
-                foreach (HtmlNode node in soldItemsDoc.DocumentNode.SelectNodes("//span[@class='s-item__price']"))
+
+                List<double> itemPrices = new List<double>();
+                List<string> itemTitles = new List<string>();
+                var sem = new SemaphoreSlim(maxThreads);
+
+                async Task ScrapePage(int page)
                 {
-                    string priceString = node.InnerText.Trim().Replace("$", "").Replace(",", "");
-                    double price;
-                    if (Double.TryParse(priceString, out price))
+                    await sem.WaitAsync();
+                    try
                     {
-                        soldItemPrices.Add(price);
+                        var response = await client.GetAsync($"{ebayUrl}&_pgn={page}");
+                        response.EnsureSuccessStatusCode();
+                        string ebayHtml = await response.Content.ReadAsStringAsync();
+
+                        HtmlDocument ebayDoc = new HtmlDocument();
+                        ebayDoc.LoadHtml(ebayHtml);
+
+                        foreach (HtmlNode node in ebayDoc.DocumentNode.SelectNodes("//span[@class='s-item__price']"))
+                        {
+                            string priceString = node.InnerText.Trim().Replace("$", "").Replace(",", "");
+                            double price;
+                            if (Double.TryParse(priceString, out price) && !Double.IsNaN(price))
+                            {
+                                lock (itemPrices)
+                                {
+                                    itemPrices.Add(price);
+                                }
+                            }
+                        }
+
+                        foreach (HtmlNode node in ebayDoc.DocumentNode.SelectNodes("//div[@class='s-item__title']"))
+                        {
+                            string title = node.InnerText.Trim();
+                            lock (itemTitles)
+                            {
+                                itemTitles.Add(title);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        sem.Release();
                     }
                 }
-                
-                foreach (double price in soldItemPrices)
+
+                var tasks = new List<Task>();
+
+                for (int i = 1; i <= pageNum; i++)
                 {
-                    tbSettingText.Text += price.ToString() + Environment.NewLine;
+                    tasks.Add(ScrapePage(i));
+                }
+
+                await Task.WhenAll(tasks);
+
+                if (itemPrices.Count == 0)
+                {
+                    tbSettingText.Text = "No results found.";
+                }
+                else
+                {
+                    for (int i = 1; i < itemPrices.Count; i++)
+                    {
+                        tbSettingText.Text += itemTitles[i] + " - $" + itemPrices[i].ToString("0.00") + Environment.NewLine;
+                    }
                 }
             }
         }
